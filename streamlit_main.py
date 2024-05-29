eec1a9af300400f9a8676a706fc3cb8124edc51b
Travis Liang <68076706+liangtravis510@users.noreply.github.com>
2024-05-29 12:41:03 -0700

    Update streamlit_main.py

--- streamlit_main.py
+++ streamlit_main.py
@@ -14,13 +14,13 @@
 #     go_to_page()
 
 # ------------------------------------------------
-from sklearn.impute import SimpleImputer
 import streamlit as st
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
+from sklearn.impute import SimpleImputer
 import matplotlib.pyplot as plt
 import seaborn as sns
 
